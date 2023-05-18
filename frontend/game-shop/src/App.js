ba8f9043b1734039b902774797086796dedfa69a
hitmonchan <dominiccpeterson@gmail.com>
2023-05-18 01:29:07 -0400

    barebones/basic cart functionality

--- frontend/game-shop/src/App.js
+++ frontend/game-shop/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 const App = () => {
   const [games, setGames] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/api/games")
@@ -11,13 +12,36 @@ const App = () => {
       .then((data) => setGames(data));
   }, []);
 
-  const handleBuy = () => {
+  const handleBuy = (id) => {
+    console.log(id);
     console.log("add to cart");
+    fetch(`http://localhost:3001/api/cart/add-to-cart/?userId=${userId}`, {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDY0MTFhMGM5MzEyYTYxMjdkYjA3NjkiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2ODQzODcyODAsImV4cCI6MTY4NDQ3MzY4MH0.oGbVKLLvt-3zq1yaMaIza7kZ3QVFh3MGtW_Gd8C-S7E",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(id),
+    }).then(() =>
+      fetch(`http://localhost:3001/api/cart/?userId=${userId}`, {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDY0MTFhMGM5MzEyYTYxMjdkYjA3NjkiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2ODQzODcyODAsImV4cCI6MTY4NDQ3MzY4MH0.oGbVKLLvt-3zq1yaMaIza7kZ3QVFh3MGtW_Gd8C-S7E",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => setCart(data))
+    );
   };
 
   if (!games) {
     return null;
   }
+
+  const userId = "646411a0c9312a6127db0769";
 
   return (
     <>
@@ -25,9 +49,16 @@ const App = () => {
         <article className="mb-5" key={`game-${game.name}`}>
           <span className="d-block mb-2">name: {game.name}</span>
           <span className="d-block mb-4">price: {game.price}</span>
-          <button onClick={handleBuy}>Buy</button>
+          <button onClick={() => handleBuy([game._id])}>Buy</button>
         </article>
       ))}
+
+      {!!cart.games?.length &&
+        cart.games.map((game) => (
+          <article>
+            <span>{game.name}</span>
+          </article>
+        ))}
     </>
   );
 };
